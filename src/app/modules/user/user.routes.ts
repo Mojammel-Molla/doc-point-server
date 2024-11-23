2f3504870c970abe01b940db67dc8380a371715b
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-11-24 00:00:02 +0600

    File upload in cloud complete

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,12 +3,14 @@ import { userControllers } from './user.controller';
 
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
+import { fileUploader } from '../../helpers/fileUploader';
 
 const router = express.Router();
 
 router.post(
   '/',
   auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+  fileUploader.upload.single('file'),
   userControllers.createAdmin
 );
 
